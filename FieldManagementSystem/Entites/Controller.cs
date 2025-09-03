6675b09d9d1d3eeee5dfaa353ff41da2345adc18
Moaad Amer <moaad02@gmail.com>
2025-09-03 10:56:15 +0300

    Create the entites

---
+++ FieldManagementSystem/Entites/Controller.cs
@@ -0,0 +1,9 @@
+﻿namespace FieldManagementSystem.Entites
+{
+    public class Controller
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public Guid FieldId { get; set; }
+    }
+}
---
+++ FieldManagementSystem/Entites/Field.cs
@@ -0,0 +1,10 @@
+﻿namespace FieldManagementSystem.Entites
+{
+    public class Field
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
